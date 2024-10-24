4dc28b228fe62c74da736d26484278807cda4426
atsalrizandridev <atsalrizandrii@gmail.com>
2024-10-24 12:55:00 +0700

    🎉 feat: initial commit – project setup complete

---
+++ app/src/main/java/com/atsal/bmw_first_submission/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.atsal.bmw_first_submission
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
